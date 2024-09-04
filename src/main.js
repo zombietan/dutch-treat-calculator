c9f16d381633457efec994952e706729cdd5adcd
zombietan <fatcat7@aol.jp>
2024-09-04 09:31:12 +0900

    migrate project from Vue2 to Vue3

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 
-Vue.config.productionTip = false
 
-new Vue({
-  store,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+createApp(App)
+  .use(store)
+  .mount('#app')
